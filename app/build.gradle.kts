b2501da4fe24be33b9ef5e5614b15b6fd8e5673e
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 04:56:43 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.2.0" apply false
+    kotlin("android") version "1.9.10" apply false
 }
 
 android {
